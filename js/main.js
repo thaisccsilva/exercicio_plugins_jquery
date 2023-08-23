abb6f9a533f9e92b1a1df5cfb8cd449bce25505f
Thais C C e Silva <thaisccs@gmail.com>
2023-08-23 17:56:28 -0300

    Commit de correção

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 	$('#telefone').mask('(00) 00000-0000');
 	$('#cep').mask('00000-000'); 
 	$('#cpf').mask('000.000.000-00'); 
-
     $('form').validate({
         rules: {
             nome: {
